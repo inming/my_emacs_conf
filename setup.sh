9c9c912d7fe27fc0d0c1eb280abd1a16df8c2b7d
Ming Yin <inming521@gmail.com>
2016-03-01 17:38:08 +0800

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -6,19 +6,6 @@ install_script() {
     { [ ! -e "$2" ] && ln -s "$SCRIPT_PATH/$1" "$2" && echo "$2 installed"; } || echo "$2 NOT installed"
 }
 
-
-if [ "`uname`" = "Darwin" ] ; then
-    install_script emacs/emacs.el ~/.emacs
-    install_script emacs/emacs.d ~/.emacs.d
-    mkdir -p ~/.backup
-    install_script emacs/template ~/.template
-    install_script emacs/tmthemes ~/.tmthemes
-fi
-
-if [ "`uname`" = "GNU/Linux" ] ; then
-    install_script emacs/emacs.el ~/.emacs
-    install_script emacs/emacs.d ~/.emacs.d
-    mkdir -p ~/.backup
-    install_script emacs/template ~/.template
-    install_script emacs/tmthemes ~/.tmthemes
-fi
+install_script emacs.el ~/.emacs
+install_script emacs.d ~/.emacs.d
+mkdir -p ~/.backup
